6ec31c837fc909a6b23e71c97ee73b67fe375967
ateru0501 <syotaroara@gmail.com>
2021-11-17 12:05:03 +0000

    first commit

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,14 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+// 14章追記,Profile Modelが扱えるようになる
+use App\Profile;
+
+// 17追記
+use App\ProfileHistory;
+
+use Carbon\Carbon;
+
 class ProfileController extends Controller
 {
     //
@@ -13,18 +21,55 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
     
-    public function create()
+    public function create(Request $request)
     {
+        // Varidationを行う
+        $this->validate($request,Profile::$rules);
+         
+        $profile = new Profile;
+        $form = $request->all();
+        
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+        
         return redirect('admin/profile/create');
     }
     
-    public function edit()
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
     
-    public function update()
+    public function update(Request $request)
     {
-        return redirect('admin/profile/edit');
+        // Validationをかける
+        $this->validate($request, Profile::$rules);
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        // 送信されてきたフォームデータを格納する
+        $profile_form = $request->all();
+        
+        unset($profile_form['_token']);
+        unset($profile_form['remove']);
+        
+        // 該当するデータを上書きして保存する
+        $profile->fil($profile_form)->save();
+        
+        // 17追記
+        $profile_history = new ProfileHistory();
+        $profile_history->profile_id = $profile->id;
+        $profile_history->edited_at = carbon::now();
+        $profile_history->save();
+        
+        return redirect('admin/profile/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create'); # 13章追記
+    Route::get('news', 'Admin\NewsController@index'); #15章追記
+    Route::get('news/edit', 'Admin\NewsController@edit'); #16章追記
+    Route::post('news/edit', 'Admin\NewsController@update'); #16章追記
+    Route::get('news/delete', 'Admin\NewsController@delete'); #17章追記
     
     //profile
     Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->name('profile.edit');
+    Route::post('profile/create', 'Admin\ProfileController@create'); # 13章追記
+    Route::post('profile/edit', 'Admin\ProfileController@update'); # 13章追記
 });
 
-Route::group('XXX', 'AAAController@bbb');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
